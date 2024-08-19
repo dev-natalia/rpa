9ebbc51f231e862ee9a107f8a741bf5c55340feb
Natalia <dev.nataliaps@gmail.com>
2024-08-18 23:27:39 -0300

    update task

--- tasks.py
+++ tasks.py
@@ -1,8 +1,7 @@
-from RPA.Robocorp.WorkItems import task
 from robot_script import Robot
 
-@task
 def task():
     robot = Robot()
     robot.start_robot(phrase="são paulo", time_period=0)
     
+task()
