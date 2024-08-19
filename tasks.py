fd4d2e1be7c9e0fd7aecf282f45b508ab7276c09
Natalia <dev.nataliaps@gmail.com>
2024-08-18 23:20:25 -0300

    fix task

--- tasks.py
+++ tasks.py
@@ -1,9 +1,8 @@
+from RPA.Robocorp.WorkItems import task
 from robot_script import Robot
 
+@task
 def task():
     robot = Robot()
     robot.start_robot(phrase="são paulo", time_period=0)
     
-
-if __name__ == "__main__":
-    task()
